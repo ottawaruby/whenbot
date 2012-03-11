c2234e9a4c06e398a058fdafd7c0b57c51c8df39
Bonnie Cheung <bonnie.cheung@gmail.com>
2012-03-10 19:13:17 -0500

    factories folder

---
+++ factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    username 'Whenbotter'
+    email 'whenbotter@gmail.com'
+    password 'dictionaryattack123'
+    password_confirmation 'dictionaryattack123'
+  end
+end
