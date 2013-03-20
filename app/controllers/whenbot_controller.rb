cf30fcae0d57a482c6e08b8ab8b5731fb5eeadfa
Ryan Seys <rsmseys@gmail.com>
2013-03-20 00:10:21 -0400

    return response rather than response body

--- app/controllers/whenbot_controller.rb
+++ app/controllers/whenbot_controller.rb
@@ -2,21 +2,21 @@ class WhenbotController < ApplicationController
 
   # /whenbot/:channel/:trigger/callback
   def callback
-    
+
     # ==== One-liner 13 ====
     response = validate_response response
-    
+
     if response[:head_only]
       head response[:status]  # ==== One-liner 15 ====
     else
-      render response[:type] => response[:body], 
-             :status => response[:status], 
+      render response[:type] => response[:body],
+             :status => response[:status],
              :layout => false
     end
   end
 
   private
-  
+
   def validate_response(response)
     response ||= {}
     response[:head_only]  ||= response[:body] ? false : true
@@ -24,8 +24,6 @@ class WhenbotController < ApplicationController
     response[:type]       ||= :json
     response[:headers]    ||= ''
     response[:body]       ||= 'Success'
-    # ==== One-liner 17 ====
+    return response
   end
-  
-
 end
